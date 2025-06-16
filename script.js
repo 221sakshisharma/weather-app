bda68b7ef62bd6d7239833ae2398ef8d2d8aa1bb
Sakshi Sharma <221sakshisharma@gmail.com>
2025-06-16 19:30:21 +0530

    added default background

--- script.js
+++ script.js
@@ -20,193 +20,195 @@ const apiKey = "36961022d8f534feb5b95f14be590dc6";
 let currCity = await getCity();
 
 const getTimeFromOffset = (offsetInSeconds) => {
-  const now = new Date();
-  const localTime = new Date(now.getTime() + offsetInSeconds * 1000);
-  localTime.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  });
+    const now = new Date();
+    const localTime = new Date(now.getTime() + offsetInSeconds * 1000);
+    localTime.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
+    });
 };
 
 const updateCurrentDate = () => {
-  const updateDate = () => {
-    const date = new Date();
-    const time = date.toLocaleString("en-US", {
-      hour: "numeric",
-      minute: "2-digit",
-      hour12: true,
-    });
+    const updateDate = () => {
+        const date = new Date();
+        const time = date.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+        });
 
-    // Format date
-    const formattedDate = `${time} - ${date.toLocaleString("en-US", {
-      weekday: "long",
-    })}, ${date.getDate()} ${date.toLocaleString("en-US", {
-      month: "short",
-    })} ${date.getFullYear()}`;
-    dateVal.forEach((x) => {
-      x.innerText = formattedDate;
-    });
-  };
+        // Format date
+        const formattedDate = `${time} - ${date.toLocaleString("en-US", {
+            weekday: "long",
+        })}, ${date.getDate()} ${date.toLocaleString("en-US", {
+            month: "short",
+        })} ${date.getFullYear()}`;
+        dateVal.forEach((x) => {
+            x.innerText = formattedDate;
+        });
+    };
 
-  updateDate();
-  setInterval(updateDate, 60000);
+    updateDate();
+    setInterval(updateDate, 60000);
 };
 
 async function getData(endPoint) {
-  const apiURL = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${currCity}&appid=${apiKey}&units=metric`;
+    const apiURL = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${currCity}&appid=${apiKey}&units=metric`;
 
-  console.log(apiURL);
+    console.log(apiURL);
 
-  try {
-    const response = await fetch(apiURL);
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching weather data:", error);
-  }
+    try {
+        const response = await fetch(apiURL);
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
 }
 
 const getTemperatureImg = (id) => {
-  if (id <= 232) return "thunderstorms";
-  else if (id <= 321) return "drizzle";
-  else if (id <= 531) return "rain";
-  else if (id <= 622) return "snow";
-  else if (id <= 781) return "atmosphere";
-  else if (id === 800) return "clear";
-  else return "clouds";
+    if (id <= 232) return "thunderstorms";
+    else if (id <= 321) return "drizzle";
+    else if (id <= 531) return "rain";
+    else if (id <= 622) return "snow";
+    else if (id <= 781) return "atmosphere";
+    else if (id === 800) return "clear";
+    else return "clouds";
 };
 
 const updateWeatherUI = async () => {
-  const weatherData = await getData("weather");
+    const weatherData = await getData("weather");
 
-  if (weatherData.cod != 200) {
-    alert("Enter a valid city");
-    return;
-  }
+    if (weatherData.cod != 200) {
+        alert("Enter a valid city");
+        return;
+    }
 
-  const {
-    name: city,
-    main: { temp, humidity },
-    weather: [{ id, main }],
-    wind: { speed },
-    timezone,
-  } = weatherData;
+    const {
+        name: city,
+        main: { temp, humidity },
+        weather: [{ id, main }],
+        wind: { speed },
+        timezone,
+    } = weatherData;
 
-  tempVal.innerText = temp.toFixed(1);
-  humidityVal.innerText = humidity;
-  windSpeedVal.innerText = speed;
-  if (city.length >= 8) {
-    locationVal[0].style.fontSize = "75px"; // or any smaller size you prefer
-  } else {
-    locationVal[0].style.fontSize = "100px"; // default size
-  }
-  locationVal.forEach((x) => {
-    x.innerText = city;
-  });
-  weatherVal.innerText = main;
+    tempVal.innerText = temp.toFixed(1);
+    humidityVal.innerText = humidity;
+    windSpeedVal.innerText = speed;
+    if (city.length >= 8) {
+        locationVal[0].style.fontSize = "75px"; // or any smaller size you prefer
+        locationVal[0].style.lineHeight = "60px";
+    } else {
+        locationVal[0].style.fontSize = "100px"; // default size
+        locationVal[0].style.lineHeight = "80px";
+    }
+    locationVal.forEach((x) => {
+        x.innerText = city;
+    });
+    weatherVal.innerText = main;
 
-  const imageUrl = await getCityPhotoURL(city);
-  const img = new Image();
-  img.src = imageUrl;
+    const imageUrl = await getCityPhotoURL(city);
+    const img = new Image();
+    img.src = imageUrl;
 
-  console.log(imageUrl);
+    console.log(imageUrl);
 
-  // Wait for the image to fully load before applying as background
-  img.onload = () => {
-    document.body.style.backgroundImage = `url("${imageUrl}")`;
-    // document.body.style.backgroundSize = "cover";
-    // document.body.style.backgroundPosition = "center";
-    console.log("Background image successfully set.");
-  };
+    // Wait for the image to fully load before applying as background
+    img.onload = () => {
+        document.body.style.backgroundImage = `url("${imageUrl}")`;
+        // document.body.style.backgroundSize = "cover";
+        // document.body.style.backgroundPosition = "center";
+        console.log("Background image successfully set.");
+    };
 
-  img.onerror = () => {
-    console.error("Failed to load background image:", imageUrl);
-  };
+    img.onerror = () => {
+        console.error("Failed to load background image:", imageUrl);
+    };
 
-  temperatureImg.setAttribute(
-    "src",
-    `assets/weather/${getTemperatureImg(id)}.svg`
-  );
-  temperatureImg.setAttribute("alt", main);
+    temperatureImg.setAttribute(
+        "src",
+        `assets/weather/${getTemperatureImg(id)}.svg`
+    );
+    temperatureImg.setAttribute("alt", main);
 
-  await updateForecastUI();
+    await updateForecastUI();
 
-  noCityFoundContainer.style.display = "none";
-  weatherSummary.style.display = "block";
+    noCityFoundContainer.style.display = "none";
+    weatherSummary.style.display = "block";
 };
 
 const updateForecastUI = async () => {
-  const forecastData = await getData("forecast");
+    const forecastData = await getData("forecast");
 
-  if (forecastData.cod != 200) {
-    forecastSection.style.display = "none";
-    return;
-  }
+    if (forecastData.cod != 200) {
+        forecastSection.style.display = "none";
+        return;
+    }
 
-  const timeTaken = "12:00:00";
+    const timeTaken = "12:00:00";
 
-  const todayDate = new Date().toISOString().split("T")[0];
+    const todayDate = new Date().toISOString().split("T")[0];
 
-  forecastSection.innerHTML = "";
+    forecastSection.innerHTML = "";
 
-  let n = 4;
+    let n = 4;
 
-  forecastData.list.forEach((forecastWeather) => {
-    if (
-      n != 0 &&
-      forecastWeather.dt_txt.includes(timeTaken) &&
-      !forecastWeather.dt_txt.includes(todayDate)
-    ) {
-      n--;
+    forecastData.list.forEach((forecastWeather) => {
+        if (
+            n != 0 &&
+            forecastWeather.dt_txt.includes(timeTaken) &&
+            !forecastWeather.dt_txt.includes(todayDate)
+        ) {
+            n--;
 
-      const {
-        main: { temp },
-        weather: [{ id, main }],
-        dt_txt: dateString,
-      } = forecastWeather;
+            const {
+                main: { temp },
+                weather: [{ id, main }],
+                dt_txt: dateString,
+            } = forecastWeather;
 
-      const actualDate = new Date(dateString);
+            const actualDate = new Date(dateString);
 
-      const formattedDate = `${actualDate.getDate()} ${actualDate.toLocaleString(
-        "en-US",
-        { month: "short" }
-      )}`;
+            const formattedDate = `${actualDate.getDate()} ${actualDate.toLocaleString(
+                "en-US",
+                { month: "short" }
+            )}`;
 
-      forecastSection.innerHTML += `
+            forecastSection.innerHTML += `
             <div class="forecast-container">
                     <img src="assets/weather/${getTemperatureImg(
-                      id
-                    )}.svg" alt="${main}" class="temperature-img small">
+                id
+            )}.svg" alt="${main}" class="temperature-img small">
                     <div class="day-info">
                         <p class="day-date">${formattedDate}</p>
                         <p class="day-temperature-info"><span class="day-temperature">${temp.toFixed(
-                          1
-                        )}</span>° C</p>
+                1
+            )}</span>° C</p>
                     </div>
                 </div>
             `;
-    }
-  });
+        }
+    });
 };
 
 const handleSearch = async () => {
-  if (cityInput.value.trim() !== "") {
-    currCity = cityInput.value.trim().toLowerCase();
-    await updateWeatherUI();
-  }
-  cityInput.value = "";
-  cityInput.blur();
+    if (cityInput.value.trim() !== "") {
+        currCity = cityInput.value.trim().toLowerCase();
+        await updateWeatherUI();
+    }
+    cityInput.value = "";
+    cityInput.blur();
 };
 
 searchBtn.addEventListener("click", handleSearch);
 
 currentLocationBtn.addEventListener("click", async () => {
-  currCity = await getCity();
-  await updateWeatherUI();
+    currCity = await getCity();
+    await updateWeatherUI();
 });
 
 cityInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") handleSearch();
+    if (e.key === "Enter") handleSearch();
 });
 
 updateCurrentDate();
